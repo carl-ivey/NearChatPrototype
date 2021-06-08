f011dad0b6c9a964505dd413e27bb0ef605f99c4
javadev115 <coxscott64@gmail.com>
2021-06-07 22:22:55 -0700

    Added layouts for login, registration and profile details screens

---
+++ NearChatAndroidApp/app/src/main/java/com/natusvincere/nearchat/RegisterAcctScreen.java
@@ -0,0 +1,13 @@
+package com.natusvincere.nearchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterAcctScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_acct_screen);
+    }
+}
